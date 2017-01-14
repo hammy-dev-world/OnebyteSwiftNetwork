e661fc0c9997f98a4aec501f429a0838e14b32f6
Humayun Sohail <humayun4206@gmail.com>
2017-01-14 14:01:48 +0500

    without pods

--- Example/Pods/Target Support Files/OnebyteSwiftNetworkCycle/OnebyteSwiftNetworkCycle-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_OnebyteSwiftNetworkCycle : NSObject
-@end
-@implementation PodsDummy_OnebyteSwiftNetworkCycle
-@end
--- Example/Pods/Target Support Files/OnebyteSwiftNetworkCycle/OnebyteSwiftNetworkCycle-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double OnebyteSwiftNetworkCycleVersionNumber;
-FOUNDATION_EXPORT const unsigned char OnebyteSwiftNetworkCycleVersionString[];
-
