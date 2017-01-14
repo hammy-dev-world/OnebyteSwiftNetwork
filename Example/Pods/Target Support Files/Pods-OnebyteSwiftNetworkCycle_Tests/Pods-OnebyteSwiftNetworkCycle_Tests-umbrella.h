2780e6e5b8561b7e949a8a7facfb5fa95f4807bf
Humayun Sohail <humayun4206@gmail.com>
2017-01-14 14:53:39 +0500

    pods

--- Example/Pods/Target Support Files/Pods-OnebyteSwiftNetworkCycle_Tests/Pods-OnebyteSwiftNetworkCycle_Tests-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_OnebyteSwiftNetworkCycle_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_OnebyteSwiftNetworkCycle_TestsVersionString[];
-
